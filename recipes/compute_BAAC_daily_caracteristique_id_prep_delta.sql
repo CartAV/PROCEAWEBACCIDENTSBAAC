5d5b1c2fe5b915a81eaa6fa998eae127e1a7446c
dupontlau <dupontlau>
2019-03-14 09:28:20 +0000

    Saved recipe: PROCEAWEBACCIDENTSBAAC.compute_BAAC_daily_caracteristique_id_prep_delta

--- recipes/compute_BAAC_daily_caracteristique_id_prep_delta.sql
+++ recipes/compute_BAAC_daily_caracteristique_id_prep_delta.sql
@@ -1,5 +1,5 @@
 SELECT *
   FROM "baac_daily_caracteristique_id_prep"
   WHERE "Num_Acc" NOT IN (
-      SELECT DISTINCT "Num_Acc" FROM "baac_daily_lieu_id_prep_distinct"
+      SELECT DISTINCT "Num_Acc" "baac_daily_caracteristique_id_prep_distinct_stock"
   )
